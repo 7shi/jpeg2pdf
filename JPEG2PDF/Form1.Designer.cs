0b61edef5ebbdbfd9e3e3142c6eb49258f4fda4b
7shi <7shi@live.jp>
2011-02-05 12:22:03 +0900

    first commit

---
+++ JPEG2PDF/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace JPEG2PDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "テスト";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "test.pdf";
+            this.saveFileDialog1.Filter = "PDF ファイル (*.pdf)|*.pdf|すべてのファイル (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "JPEG to PDF converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ JPEG2PDF/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JPEG2PDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                var pdf = saveFileDialog1.FileName;
+                Test(pdf);
+                Process.Start(pdf);
+            }
+        }
+
+        private void Test(string pdf)
+        {
+            using (var fs = new FileStream(pdf, FileMode.Create))
+            using (var sw = new StreamWriter(fs) { AutoFlush = true })
+            {
+                var objp = new List<long>();
+
+                sw.WriteLine("%PDF-1.2");
+
+                sw.WriteLine();
+                objp.Add(fs.Position);
+                sw.WriteLine("1 0 obj");
+                sw.WriteLine("<< /Type /Page /Parent 3 0 R /Contents 2 0 R >>");
+                sw.WriteLine("endobj");
+
+                var s2 = new StringWriter();
+                s2.WriteLine("q");
+                s2.WriteLine("200 0 0 283 0 0 cm");
+                //s2.WriteLine("/ObjX Do");
+                s2.WriteLine("Q");
+                s2.Close();
+                var str2 = s2.ToString();
+
+                sw.WriteLine();
+                objp.Add(fs.Position);
+                sw.WriteLine("2 0 obj");
+                sw.WriteLine("<< /Length {0} >>", str2.Length);
+                sw.WriteLine("stream");
+                sw.Write(str2);
+                sw.WriteLine("endstream");
+                sw.WriteLine("endobj");
+
+                sw.WriteLine();
+                objp.Add(fs.Position);
+                sw.WriteLine("3 0 obj");
+                sw.WriteLine("<< /Type /Pages /Kids [ 1 0 R ] /Count 1 /MediaBox [ 0 0 595 842] >>");
+                sw.WriteLine("endobj");
+
+                sw.WriteLine();
+                objp.Add(fs.Position);
+                sw.WriteLine("4 0 obj");
+                sw.WriteLine("<< /Type /Catalog /Pages 3 0 R >>");
+                sw.WriteLine("endobj");
+
+                sw.WriteLine();
+                var xref = fs.Position;
+                sw.WriteLine("xref");
+                var size = objp.Count + 1;
+                sw.WriteLine("0 {0}", size);
+                sw.WriteLine("{0:0000000000} {1:00000} f", 0, 65535);
+                foreach (var p in objp)
+                    sw.WriteLine("{0:0000000000} {1:00000} n", p, 0);
+                sw.WriteLine("trailer");
+                sw.WriteLine("<< /Root 4 0 R /Size {0} >>", size);
+                sw.WriteLine("startxref");
+                sw.WriteLine("{0}", xref);
+                sw.WriteLine("%%EOF");
+            }
+        }
+    }
+}
